6cc7bda5cf451397ea515e09d4658030d5ada0e2
rhaydengh <hayden.becca@gmail.com>
2017-09-04 22:29:05 -0400

    Update assignment1_part2.py

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,52 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Rebecca Hayden, Assignment 1, Part 2"""
+"""Assignment 1, Part 2"""
 
 class Book:
     """Class for books"""
+
     def __init__(self, author, title):
         """this function returns a book by author and title based on variables"""
+
 	self.author = author
 	self.title = title
 
     def display(self):
-	"""This function provides instructions on printing format"""
-	print"{0}, written by {1}".format(self.title, self.author)}
-
-	print(self.title, 'written by' self.author)
-
-Book1 = Book('Of Mice and Men', 'John Steinbeck')
-Book2 = Book('To Kill a Mockingbird','Harper Lee')
-
-Book1.display()
-Book2.display()
-
-x.author('John Steinbeck')
-x.title('Of Mice and Men')
-
-y.author('Harper Lee')
-y.title('To Kill a Mockingbird')
+	"""This function provides instructions on printing format and displays 2 books"""
+	print(self.title, 'written by', self.author)
 
 
-to call
+    Book1 = Book('John Steinbeck', 'Of Mice and Men')
+    Book2 = Book('Harper Lee', 'To Kill a Mockingbird')
 
-x.display()
-y.display()
-
-
-
-
-		a.title
-
- title 'written by' author
-		Book1 
-
-class Book:
-    def __init__(self):
-	self.author1 = ('John Steinbeck')
-	self.title1 = ('Of Mice and Men')
-	self.author2 = ('Harper Lee')
-	self.title2 = ('To Kill a Mockingbird')
-
-    def display(self):
-	print(self.author)
+    print(Book1.display(),Book2.display())
