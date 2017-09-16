23bc5df439be2ae5e3fadaad91fa89561f425c9a
rhaydengh <hayden.becca@gmail.com>
2017-09-16 18:35:17 -0400

    Update assignment1_part1.py

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -7,9 +7,12 @@ def listDivide(numbers, divide=2):
     and returns the amount of numbers from that list that are divisible by divide"""
 
     global newlist
+    int(divide)
+    newlist = []
     for x in numbers:
-        if x%divide == 0:
-            newlist = [x/divide for x in numbers]
+        a = x/divide
+        if a == int(a):
+            newlist.append(a)
     return len(newlist)
 
 class ListDivideException(object):
@@ -19,8 +22,13 @@ class ListDivideException(object):
 
     def testListDivide(listDivide):
         """This function calls the listDivide function and passes in several test parameters"""
-        listDivide([1,2,3,4,5])
-        listDivide([2,4,6,8,10])
-        listDivide([30,54,63,98,100], divide=10)
-        listDivide([])
-        listDivide([1,2,3,4,5], 1)
+        if listDivide([1,2,3,4,5]) != 2:
+            raise ListDivideException()
+        if listDivide([2,4,6,8,10]) != 5:
+            raise ListDivideException()
+        if listDivide([30,54,63,98,100], divide=10) != 2:
+            raise ListDivideException()
+        if listDivide([]) != 0:
+            raise ListDivideException()
+        if listDivide([1,2,3,4,5], 1) != 5:
+            raise ListDivideException()
