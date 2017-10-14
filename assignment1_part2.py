dd18cd674f92fb946facdf8313b48d0fb5f2ab66
rhaydengh <hayden.becca@gmail.com>
2017-10-13 22:05:33 -0400

    Update assignment1_part2.py

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -8,15 +8,16 @@ class Book:
     def __init__(self, author, title):
         """this function returns a book by author and title based on variables"""
 
-	self.author = author
-	self.title = title
+        self.author = author
+        self.title = title
 
     def display(self):
-	"""This function provides instructions on printing format and displays 2 books"""
-	print(self.title, 'written by', self.author)
+        """This function provides instructions on printing format and displays 2 books"""
+        print(self.title, 'written by', self.author)
 
-    Book1 = Book('John Steinbeck', 'Of Mice and Men')
-    Book2 = Book('Harper Lee', 'To Kill a Mockingbird')
 
-    Book1.display()
-    Book2.display()
+Book1 = Book('John Steinbeck', 'Of Mice and Men')
+Book2 = Book('Harper Lee', 'To Kill a Mockingbird')
+
+print(Book1.display(),Book2.display())
+
